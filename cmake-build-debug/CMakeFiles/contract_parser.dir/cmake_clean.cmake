669cc5431ac417afb862a139bbf945c629114768
maaahad <ahad3112@yahoo.com>
2019-01-24 01:00:05 +0100

    trying to change the project language type to C++

---
+++ cmake-build-debug/CMakeFiles/contract_parser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/contract_parser.dir/main.cpp.o"
+  "contract_parser.pdb"
+  "contract_parser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/contract_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
